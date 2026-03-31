d601bf05917446446baf1f53f88ce22eecdc69c6
Piseratopo <buinhatminh2k52005@gmail.com>
2026-03-31 17:17:56 +0700

    Fix cors

--- Backend/main.js
+++ Backend/main.js
@@ -45,10 +45,7 @@ mongoose
 });  
 
 app.use(express.json());   
-app.use(cors({
-   origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-   credentials: true
-}));
+app.use(cors());
 
 // API Routes
 // Save a question
